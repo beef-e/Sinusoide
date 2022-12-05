97b392ca6f8c33b503aacd501d34e8695b65c8bc
Giammaria Biffi <gmb2005itabackup@gmail.com>
2022-12-05 10:46:49 +0100

    Added bootstrap and fixed horizontal scroll

--- static/js/extra.js
+++ static/js/extra.js
@@ -2,9 +2,9 @@
  *BOTTONE DI RESET
  */
 
-let resetButton = document.querySelector("#resetBTN");
+let resetButton = document.querySelector('#resetBTN');
 
-resetButton.addEventListener("click", function (e) {
+resetButton.addEventListener('click', function (e) {
 	resetting();
 });
 
@@ -12,33 +12,33 @@ resetButton.addEventListener("click", function (e) {
  *CAMBIO COLORE ALLE LINEE
  */
 
-let seno = document.querySelector("#redex");
-let coseno = document.querySelector("#greenex");
+let seno = document.querySelector('#redex');
+let coseno = document.querySelector('#greenex');
 colorSin = seno.value;
 colorCos = coseno.value;
 
-seno.addEventListener("input", function (e) {
+seno.addEventListener('input', function (e) {
 	colorSin = seno.value;
 	seno.style.backgroundColor = seno.value;
 });
 
-coseno.addEventListener("input", function (e) {
+coseno.addEventListener('input', function (e) {
 	colorCos = coseno.value;
 	coseno.style.backgroundColor = coseno.value;
 });
 
-let larghezza = document.querySelector("#width");
-let altezza = document.querySelector("#height");
+let larghezza = document.querySelector('#width');
+let altezza = document.querySelector('#height');
 w = parseInt(larghezza.value);
 h = parseInt(altezza.value);
 
-larghezza.addEventListener("input", function (e) {
+larghezza.addEventListener('input', function (e) {
 	resetting();
 	w = parseInt(larghezza.value);
 	setup();
 });
 
-altezza.addEventListener("input", function (e) {
+altezza.addEventListener('input', function (e) {
 	resetting();
 	h = parseInt(altezza.value);
 	setup();
